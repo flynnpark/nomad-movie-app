87c17e8f3f73236890c822d662f4ef31916b2fea
Flynn <wphestiraid@gmail.com>
2023-06-13 19:24:57 +0900

    :wrench: eslint, prettier 설정 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,11 +4,25 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'simple-import-sort', '@tanstack/query'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'import/newline-after-import': ['error', { count: 1 }],
+    '@tanstack/query/exhaustive-deps': 'error',
+    '@tanstack/query/prefer-query-object-syntax': 'error',
   },
-}
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+      },
+    },
+  },
+};
