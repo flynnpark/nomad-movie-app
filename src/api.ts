9c7363e66f53715448899bcda735084072a8346d
Flynn <wphestiraid@gmail.com>
2023-06-14 22:47:06 +0900

    :construction: 블루프린트에 있는 api.ts 마이그레이션

---
+++ src/api.ts
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://movies-api.nomadcoders.workers.dev';
+
+const apiClient = axios.create({ baseURL: BASE_URL });
+
+export async function getPopular() {
+  const { data } = await apiClient.get<GetMoviesResponse>('/popular');
+  return data;
+}
+
+export async function getNowPlaying() {
+  const { data } = await apiClient.get<GetMoviesResponse>('/now-playing');
+  return data;
+}
+
+export async function getComingSoon() {
+  const { data } = await apiClient.get<GetMoviesResponse>('/coming-soon');
+  return data;
+}
+
+export async function getMovie(id: string) {
+  const { data } = await apiClient.get<GetMovieDetailResponse>('/movie', {
+    params: {
+      id,
+    },
+  });
+  return data;
+}
+
+export function makeImagePath(image: string) {
+  return `https://image.tmdb.org/t/p/w500${image}`;
+}
+
+export function makeBgPath(image: string) {
+  return `https://image.tmdb.org/t/p/original${image}`;
+}
---
+++ src/types/api.d.ts
@@ -0,0 +1,66 @@
+interface IMovie {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+interface GetMovieDetailResponse extends IMovie {
+  belongs_to_collection: BelongsToCollection;
+  budget: number;
+  genres: Genre[];
+  homepage: string;
+  imdb_id: string;
+  production_companies: ProductionCompany[];
+  production_countries: ProductionCountry[];
+  revenue: number;
+  runtime: number;
+  spoken_languages: SpokenLanguage[];
+  status: string;
+  tagline: string;
+}
+
+interface BelongsToCollection {
+  backdrop_path: string;
+  id: number;
+  name: string;
+  poster_path: string;
+}
+
+interface Genre {
+  id: number;
+  name: string;
+}
+
+interface ProductionCompany {
+  id: number;
+  logo_path: string;
+  name: string;
+  origin_country: string;
+}
+
+interface ProductionCountry {
+  iso_3166_1: string;
+  name: string;
+}
+
+interface SpokenLanguage {
+  english_name: string;
+  iso_639_1: string;
+  name: string;
+}
+
+interface GetMoviesResponse {
+  page: number;
+  results: IMovie[];
+}
