0f75dba626ecd4de9d75f4aaff074f1c91b9da2c
Flynn <wphestiraid@gmail.com>
2023-06-17 00:38:54 +0900

    :lipstick: 전역 스타일 적용

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,14 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 
 import router from './router';
+import { GlobalStyle } from './styles/global';
 import { queryClient } from './utils/queryClient';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
+      <GlobalStyle />
       <RouterProvider router={router} />
     </QueryClientProvider>
   </React.StrictMode>
